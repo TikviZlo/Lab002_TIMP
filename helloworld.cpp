c45d273eb38820fdb662a00b8fc5747151402748
TikviZlo <99878768+TikviZlo@users.noreply.github.com>
2022-09-14 21:37:23 +0300

    Поменяла стиль?

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 int main(){
-std::string name; // name!
-std::cout << "Type your name: " // enter your name!
-std::cin >> name; // your name!
-std:cout << "Hello world and " << name << endl; // greetings!
+
+std::string name; //имя пользователя
+std::cout << "Type your name: ";
+std::cin >> name; //ввод имени пользователя
+std::cout << Hello world and " << std::name 
+          << endl; //приветствие с именем
 return 0;
 }
